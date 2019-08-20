6f85b21f2df2e094d3737d9585f21fe68b830175
Andrew Johnston <asjohnston@alaska.edu>
2019-08-20 12:50:49 -0400

    fix example-3 build whitespace

--- example-3/build.sh
+++ example-3/build.sh
@@ -1,10 +1,14 @@
 set -ex
 
 # install python dependencies
-pip3 install --system --requirement lambda-b/requirements.txt --target lambda-b/src/
+pip3 install --system \
+             --requirement lambda-b/requirements.txt \
+             --target lambda-b/src/
 
 # package cloudformation templates
-aws cloudformation package --s3-bucket cf-package-demo --template-file cloudformation.yaml --output-template-file packaged.yaml
+aws cloudformation package --template-file cloudformation.yaml \
+                           --s3-bucket cf-package-demo \
+                           --output-template-file packaged.yaml
 
 # deploy packaged cloudformation template
 aws cloudformation deploy --template-file packaged.yaml --stack-name cf-example-3 --capabilities CAPABILITY_NAMED_IAM
