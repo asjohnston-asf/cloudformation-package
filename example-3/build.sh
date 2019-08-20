c1245761365664c798b76ba1dae3aa79e20f9ace
Andrew Johnston <asjohnston@alaska.edu>
2019-08-20 08:43:47 -0400

    comment example-3 build.sh

--- example-3/build.sh
+++ example-3/build.sh
@@ -1,4 +1,10 @@
 set -e
+
+# install python dependencies
 pip3 install --system -r lambda-b/requirements.txt -t lambda-b/src/
+
+# package cloudformation templates
 aws cloudformation package --s3-bucket $BUCKET --template-file cloudformation.yaml --output-template-file packaged.yaml
+
+# deploy packaged cloudformation template
 aws cloudformation deploy --template-file packaged.yaml --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM
