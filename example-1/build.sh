12fb93ac80775579d5b429f0c90283f0fa910386
Andrew Johnston <asjohnston@alaska.edu>
2019-08-20 08:37:11 -0400

    comment example 1 build.sh

--- example-1/build.sh
+++ example-1/build.sh
@@ -1,6 +1,10 @@
 set -e
+
+# build and upload lambda deployment package
 cd src/
 zip -r ../package.zip *
 cd ..
 aws s3 cp package.zip s3://$BUCKET/
+
+# deploy cloudformation template
 aws cloudformation deploy --template-file cloudformation.yaml --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM --parameter-overrides Bucket=$BUCKET Key=package.zip
