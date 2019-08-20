debc967c80cce91c8bac50537ad016d7ff1e758e
Andrew Johnston <asjohnston@alaska.edu>
2019-08-20 08:49:25 -0400

    hardcode bucket, stack name in example-3 build.sh

--- example-3/build.sh
+++ example-3/build.sh
@@ -4,7 +4,7 @@ set -e
 pip3 install --system -r lambda-b/requirements.txt -t lambda-b/src/
 
 # package cloudformation templates
-aws cloudformation package --s3-bucket $BUCKET --template-file cloudformation.yaml --output-template-file packaged.yaml
+aws cloudformation package --s3-bucket cf-package-demo --template-file cloudformation.yaml --output-template-file packaged.yaml
 
 # deploy packaged cloudformation template
-aws cloudformation deploy --template-file packaged.yaml --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation deploy --template-file packaged.yaml --stack-name cf-package-demo --capabilities CAPABILITY_NAMED_IAM
