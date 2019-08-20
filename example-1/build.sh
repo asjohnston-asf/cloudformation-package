40cb246c29cb66969b2f340d3fa61ff2489bf393
Andrew Johnston <asjohnston@alaska.edu>
2019-08-20 08:45:44 -0400

    hardcode bucket name

--- example-1/build.sh
+++ example-1/build.sh
@@ -4,7 +4,7 @@ set -e
 cd src/
 zip -r ../package.zip *
 cd ..
-aws s3 cp package.zip s3://$BUCKET/
+aws s3 cp package.zip s3://cf-package-demo/
 
 # deploy cloudformation template
-aws cloudformation deploy --template-file cloudformation.yaml --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM --parameter-overrides Bucket=$BUCKET Key=package.zip
+aws cloudformation deploy --template-file cloudformation.yaml --stack-name cf-package-demo --capabilities CAPABILITY_NAMED_IAM --parameter-overrides Bucket=cf-package-demo Key=package.zip
