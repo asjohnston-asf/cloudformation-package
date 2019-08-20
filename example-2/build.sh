df347ba99d51b037482db58cf8b385128051565d
Andrew Johnston <asjohnston@alaska.edu>
2019-08-20 08:39:27 -0400

    Update build.sh

--- example-2/build.sh
+++ example-2/build.sh
@@ -1,3 +1,7 @@
 set -e
+
+# package lambda function and prepare final cloudformation template
 aws cloudformation package --s3-bucket $BUCKET --template-file cloudformation.yaml --output-template-file packaged.yaml
+
+# deploy final cloudformation template
 aws cloudformation deploy --template-file packaged.yaml --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM
