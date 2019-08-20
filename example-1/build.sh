38cf774306f11a0a09c9ebd8667d35c2595b06df
Andrew Johnston <asjohnston@alaska.edu>
2019-08-20 08:58:20 -0400

    verify example-1

--- example-1/build.sh
+++ example-1/build.sh
@@ -8,3 +8,7 @@ aws s3 cp package.zip s3://cf-package-demo/
 
 # deploy cloudformation template
 aws cloudformation deploy --template-file cloudformation.yaml --stack-name cf-package-demo --capabilities CAPABILITY_NAMED_IAM --parameter-overrides Bucket=cf-package-demo Key=package.zip
+
+# invoke lambda to verify deployment
+aws lambda invoke --function-name cf-example-1 out.txt
+cat out.txt
