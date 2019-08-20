d3a9fdaa5fca3694253fe795e44f13e97be0edb2
Andrew Johnston <asjohnston@alaska.edu>
2019-08-20 12:31:09 -0400

    update example-1 stack name

--- example-1/build.sh
+++ example-1/build.sh
@@ -7,7 +7,7 @@ cd ..
 aws s3 cp package.zip s3://cf-package-demo/
 
 # deploy cloudformation template
-aws cloudformation deploy --template-file cloudformation.yaml --stack-name cf-package-demo --capabilities CAPABILITY_NAMED_IAM --parameter-overrides Bucket=cf-package-demo Key=package.zip
+aws cloudformation deploy --template-file cloudformation.yaml --stack-name cf-example-1 --capabilities CAPABILITY_NAMED_IAM --parameter-overrides Bucket=cf-package-demo Key=package.zip
 
 # invoke lambda to verify deployment
 aws lambda invoke --function-name cf-example-1 out.txt
