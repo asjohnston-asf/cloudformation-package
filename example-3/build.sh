468d989d3cbce342a0746553ad0dd2673efb62af
Andrew Johnston <asjohnston@alaska.edu>
2019-08-20 12:30:22 -0400

    change example-3 stack name

--- example-3/build.sh
+++ example-3/build.sh
@@ -7,4 +7,4 @@ pip3 install --system -r lambda-b/requirements.txt -t lambda-b/src/
 aws cloudformation package --s3-bucket cf-package-demo --template-file cloudformation.yaml --output-template-file packaged.yaml
 
 # deploy packaged cloudformation template
-aws cloudformation deploy --template-file packaged.yaml --stack-name cf-package-demo --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation deploy --template-file packaged.yaml --stack-name cf-example-3 --capabilities CAPABILITY_NAMED_IAM
